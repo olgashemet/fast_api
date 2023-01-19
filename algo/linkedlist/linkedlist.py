c17182adc76d3d3785beb1733088dbb8454409eb
Alexander Sidorov <alexander@sidorov.dev>
2023-01-19 20:00:53 +0300

    set up tests

---
+++ algo/linkedlist/linkedlist.py
@@ -0,0 +1,2 @@
+class SingleLinkedList:
+    pass
---
+++ algo/linkedlist/linkedlist_test.py
@@ -0,0 +1,6 @@
+from algo.linkedlist.linkedlist import SingleLinkedList
+
+
+def test_single_linkedlist_create():
+    lst = SingleLinkedList()
+    assert lst is not None
