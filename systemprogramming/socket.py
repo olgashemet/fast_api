1246efd37111a1d01daee760bf3cee6d28ed2d0d
vshemet <volha.shemet@zalando.de>
2023-02-25 06:32:22 +0100

    add tests and format

--- systemprogramming/socket.py
+++ systemprogramming/socket.py
@@ -1,18 +1,21 @@
-def send_message(m):
+from typing import Any
+
+
+def send_message(m: Any) -> None:
     f = open("shared.txt", "w")
     f.write(m)
     f.flush()
     f.close()
 
 
-def get_message():
+def get_message() -> None:
     f = open("shared.txt", "r")
     f.seek(0)
     message = f.read()
     print(message)
 
 
-def menu():
+def menu() -> None:
     while True:
         print("1 - rec, 2-send, ... - exit")
         choice = input("your choice: ")
