2bef7e5253ff753cf089677e1ebad9bd4d735f03
Alexander Sidorov <3865736+tgrx@users.noreply.github.com>
2023-04-02 13:30:53 +0300

    fixed linters

--- examples/classes.py
+++ examples/classes.py
@@ -1,14 +1,14 @@
-def f(arg1: float, arg2: float) -> float:
-    return arg1 / arg2
-
-
+# def func(arg1: float, arg2: float) -> float:
+#     return arg1 / arg2
+#
+#
 # такой словарь, что:
 # 1. ключи - только строки
 # 2. строки состоят из [a-zA-Z_]{1}[a-zA-Z0-9_]*
 # ns = {
 #     "__doc__": "Hello world from T",
 #     "a": 1234,
-#     "__truediv__": f,
+#     "__truediv__": func,
 # }
 # T = type("T", (), ns)
 # print(T.__truediv__)
@@ -17,16 +17,16 @@ def f(arg1: float, arg2: float) -> float:
 class T:
     """Hello world from T"""
 
-    a = 1000
+    attr = 1000
 
-    def g(self, arg2, arg3) -> float:
+    def gg(self, arg2: float, arg3: float) -> float:
         print(f"{self=!r}")  # noqa: T201
         print(f"{arg2=!r}")  # noqa: T201
         print(f"{arg3=!r}")  # noqa: T201
-        return arg2 / arg3 + self.a
+        return arg2 / arg3 + self.attr
 
 
-# TODO:
+# TODO:  # noqa
 # 1. partial
 # 2. MRO
 # 3. type
