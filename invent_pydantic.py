249991277cf99e252c2079eed8c601030d391cb7
Alexander Sidorov <3865736+tgrx@users.noreply.github.com>
2023-04-30 16:28:32 +0300

    invented pydantic

---
+++ invent_pydantic.py
@@ -0,0 +1,30 @@
+from devtools import debug
+
+
+class DTO:
+    def __init__(self, **kw: dict) -> None:
+        debug(DTO.__annotations__)
+        debug(DTO.__init__.__annotations__)
+        debug(self.__class__)
+        debug(self.__class__.__annotations__)
+
+        anno = self.__class__.__annotations__
+        extra = kw.keys() - anno.keys()
+        if extra:
+            raise TypeError(f"unexpected params: {sorted(extra)}")
+
+        for arg, value in kw.items():
+            anno_type = anno[arg]
+            if not isinstance(value, anno_type):
+                raise TypeError(f"arg `{arg=!r}` expected {anno_type}, got {type(value)}")
+
+            setattr(self, arg, value)
+
+
+class User(DTO):
+    name: str
+    phone: str
+
+
+a = User(name="n", phone="p", x=123)
+debug(a.name, a.phone)
